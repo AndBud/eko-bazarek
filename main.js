af4122ac415ec577d97da41388f9ff91b19e0b61
AndBud <andbudz@gmail.com>
2023-08-20 10:23:07 +0200

    added error handler to fetch api and fixed some mistakes with onClick implementation(didn't work with vegeOnly b4)

--- main.js
+++ main.js
@@ -22,55 +22,121 @@ getTypes().then((types) => {
     element.onclick = function () {
       if (element.classList.contains("btnTypeActive")) {
         productsList.innerHTML = "";
-        getProducts(
-          "https://api-eko-bazarek.azurewebsites.net/api/products/categories"
-        ).then((products) => {
-          products.sort((a, b) => {
-            const nameA = a.name.toUpperCase();
-            const nameB = b.name.toUpperCase();
-            if (nameA < nameB) {
-              return -1;
-            }
-            if (nameA > nameB) {
-              return 1;
-            }
-            return 0;
-          });
-          products.forEach((product) => {
-            let element = document.createElement("div");
-            element.classList.add("blockCategories");
-            element.innerHTML = `<div class="${product.type}" id="${product.id}">
+        if (vegeOnly.checked) {
+          getProducts(
+            "https://api-eko-bazarek.azurewebsites.net/api/products/categories"
+          ).then((products) => {
+            products.sort((a, b) => {
+              const nameA = a.name.toUpperCase();
+              const nameB = b.name.toUpperCase();
+              if (nameA < nameB) {
+                return -1;
+              }
+              if (nameA > nameB) {
+                return 1;
+              }
+              return 0;
+            });
+            products.forEach((product) => {
+              if (
+                product.type === "MEAT" ||
+                product.type === "FISH" ||
+                product.type === "COOKED_MEATS"
+              ) {
+              } else {
+                let element = document.createElement("div");
+                element.classList.add("blockCategories");
+                element.innerHTML = `<div class="${product.type}" id="${product.id}">
           <img src="${product.iconUrl}" alt="${product.name}">
           <p class="desc">${product.name}</p>`;
-            productsList.appendChild(element);
+                productsList.appendChild(element);
+              }
+            });
           });
-        });
+        } else {
+          getProducts(
+            "https://api-eko-bazarek.azurewebsites.net/api/products/categories"
+          ).then((products) => {
+            products.sort((a, b) => {
+              const nameA = a.name.toUpperCase();
+              const nameB = b.name.toUpperCase();
+              if (nameA < nameB) {
+                return -1;
+              }
+              if (nameA > nameB) {
+                return 1;
+              }
+              return 0;
+            });
+            products.forEach((product) => {
+              let element = document.createElement("div");
+              element.classList.add("blockCategories");
+              element.innerHTML = `<div class="${product.type}" id="${product.id}">
+          <img src="${product.iconUrl}" alt="${product.name}">
+          <p class="desc">${product.name}</p>`;
+              productsList.appendChild(element);
+            });
+          });
+        }
         element.classList.remove("btnTypeActive");
       } else {
-        getProducts(
-          `https://api-eko-bazarek.azurewebsites.net/api/products/categories?type=${type.id}`
-        ).then((products) => {
-          productsList.innerHTML = "";
-          products.sort((a, b) => {
-            const nameA = a.name.toUpperCase();
-            const nameB = b.name.toUpperCase();
-            if (nameA < nameB) {
-              return -1;
-            }
-            if (nameA > nameB) {
-              return 1;
-            }
-            return 0;
+        productsList.innerHTML = "";
+        if (vegeOnly.checked) {
+          getProducts(
+            `https://api-eko-bazarek.azurewebsites.net/api/products/categories?type=${type.id}`
+          ).then((products) => {
+            products.sort((a, b) => {
+              const nameA = a.name.toUpperCase();
+              const nameB = b.name.toUpperCase();
+              if (nameA < nameB) {
+                return -1;
+              }
+              if (nameA > nameB) {
+                return 1;
+              }
+              return 0;
+            });
+            products.forEach((product) => {
+              if (
+                product.type === "MEAT" ||
+                product.type === "FISH" ||
+                product.type === "COOKED_MEATS"
+              ) {
+              } else {
+                let element = document.createElement("div");
+                element.classList.add("blockCategories");
+                element.innerHTML = `<div class="${product.type}" id="${product.id}">
+          <img src="${product.iconUrl}" alt="${product.name}">
+          <p class="desc">${product.name}</p>`;
+                productsList.appendChild(element);
+              }
+            });
           });
-          products.forEach((product) => {
-            let element = document.createElement("div");
-            element.classList.add("blockCategories");
-            element.innerHTML = `<div class="${product.type}" id="${product.id}">
-              <img src="${product.iconUrl}" alt="${product.name}">
-              <p class="desc">${product.name}</p>`;
-            productsList.appendChild(element);
+        } else {
+          getProducts(
+            `https://api-eko-bazarek.azurewebsites.net/api/products/categories?type=${type.id}`
+          ).then((products) => {
+            products.sort((a, b) => {
+              const nameA = a.name.toUpperCase();
+              const nameB = b.name.toUpperCase();
+              if (nameA < nameB) {
+                return -1;
+              }
+              if (nameA > nameB) {
+                return 1;
+              }
+              return 0;
+            });
+            products.forEach((product) => {
+              let element = document.createElement("div");
+              element.classList.add("blockCategories");
+              element.innerHTML = `<div class="${product.type}" id="${product.id}">
+          <img src="${product.iconUrl}" alt="${product.name}">
+          <p class="desc">${product.name}</p>`;
+              productsList.appendChild(element);
+            });
           });
-        });
+        }
         typesList.childNodes.forEach((type) => {
           type.classList.remove("btnTypeActive");
         });
@@ -109,6 +175,9 @@ const meats = ["MEAT", "FISH", "COOKED_MEATS"];
 
 vegeOnly.addEventListener("change", (event) => {
   productsList.innerHTML = "";
+  typesList.childNodes.forEach((type) => {
+    type.classList.remove("btnTypeActive");
+  });
   if (vegeOnly.checked) {
     getProducts(
       "https://api-eko-bazarek.azurewebsites.net/api/products/categories"
@@ -168,13 +237,37 @@ vegeOnly.addEventListener("change", (event) => {
 });
 
 async function getTypes() {
-  const response = await fetch(
-    "https://api-eko-bazarek.azurewebsites.net/api/products/types"
-  );
-  return response.json();
+  try {
+    const response = await fetch(
+      "https://api-eko-bazarek.azurewebsites.net/api/products/types"
+    );
+    if (response.status === 404) {
+      throw new Error("Page not found");
+    } else if (response.status === 500) {
+      throw new Error("Server error");
+    } else if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    const data = response.json();
+    return data;
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 async function getProducts(url) {
-  const response = await fetch(url);
-  return response.json();
+  try {
+    const response = await fetch(url);
+    if (response.status === 404) {
+      throw new Error("Page not found");
+    } else if (response.status === 500) {
+      throw new Error("Server error");
+    } else if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    const data = response.json();
+    return data;
+  } catch (error) {
+    console.error(error);
+  }
 }
